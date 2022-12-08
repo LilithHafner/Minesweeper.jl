e3b6ec8edadabe382d258d050ccedf3912706119
Documenter.jl <documenter@juliadocs.github.io>
2022-12-08 10:04:59 +0000

    build based on db58d7f

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = Minesweeper","category":"page"},{"location":"#Minesweeper","page":"Home","title":"Minesweeper","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for Minesweeper.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [Minesweeper]","category":"page"}]
+}
